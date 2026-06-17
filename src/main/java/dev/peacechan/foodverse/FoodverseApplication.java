161be8a64b6a34dd8e92fc09e756f5f0941fdf27
Chan <57628389+peacechan11@users.noreply.github.com>
2026-06-17 12:00:00 +0700

    init : Spring Boot project

---
+++ src/main/java/dev/peacechan/foodverse/FoodverseApplication.java
@@ -0,0 +1,13 @@
+package dev.peacechan.foodverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodverseApplication.class, args);
+	}
+
+}
